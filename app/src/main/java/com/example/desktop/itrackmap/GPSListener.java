4baf26a6ecd256d0224f4fedbd6b0e69ce98b36b
andreyuryukin <andrey.uryukin@gmail.com>
2016-05-25 11:41:28 +0300

    commit

---
+++ app/src/main/java/com/example/desktop/itrackmap/GPSListener.java
@@ -0,0 +1,98 @@
+package com.example.desktop.itrackmap;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+public class GPSListener implements LocationListener {
+
+    public ITrackMapActivity iTrackMapActivity;
+    public Location initialLocation;
+    public Location prevLocation;
+
+    public int accuracyDepth;
+    public boolean isBetterLocation;
+    public static final int ONE_MINUTE = 1000 * 60;
+    public static final int ACCURACY_ATTEMPTS = 7;
+
+    public GPSListener(ITrackMapActivity act, Location initLoc) {
+
+        iTrackMapActivity = act;
+        initialLocation = initLoc;
+        accuracyDepth = 0;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        if (accuracyDepth < ACCURACY_ATTEMPTS) {
+            initialLocation = location;
+            prevLocation = location;
+            accuracyDepth = accuracyDepth + 1;
+        } else if (initialLocation != null && location != null) {
+
+            isBetterLocation = isBetterLocation(prevLocation, location);
+
+            if (isBetterLocation) {
+                prevLocation = location;
+                iTrackMapActivity.updateMap(initialLocation, location);
+            }
+        }
+    }
+
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    public boolean isBetterLocation(Location currentBestLocation, Location location) {
+
+        if (currentBestLocation == null) {
+            // A new location is always better than no location
+            return true;
+        }
+
+        // Check whether the new location fix is newer or older
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > ONE_MINUTE;
+        boolean isSignificantlyOlder = timeDelta < -ONE_MINUTE;
+        boolean isNewer = timeDelta > 0;
+
+        // If it's been more than two minutes since the current location, use the new location
+        // because the user has likely moved
+        if (isSignificantlyNewer) {
+            return true;
+            // If the new location is more than two minutes older, it must be worse
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        // Check whether the new location fix is more or less accurate
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 100;
+
+        // Determine location quality using a combination of timeliness and accuracy
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else if (isNewer && !isSignificantlyLessAccurate) {
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ app/src/main/java/com/example/desktop/itrackmap/ITrackMapActivity.java
@@ -0,0 +1,124 @@
+package com.example.desktop.itrackmap;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ITrackMapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    public GoogleMap mMap;
+    public LocationManager manager;
+    public GPSListener listener;
+    public Location initialLocation;
+    public Location mostUpdateLocation;
+    public LatLng latLng;
+    public String coordinates;
+
+    private static final float ZOOM_LEVEL = 16;
+    private static final int REQUEST_LOCATION = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_itrack_map);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        initialLocation = getInitLocation();
+
+        listener = new GPSListener(ITrackMapActivity.this, initialLocation);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, listener);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (initialLocation != null) {
+            latLng = new LatLng(initialLocation.getLatitude(), initialLocation.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Israel"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, ZOOM_LEVEL));
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_LOCATION:
+                LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                if (manager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+                        || manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) break;
+                else break;
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        if (manager != null) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            manager.removeUpdates(listener);
+        }
+    }
+
+    public Location getInitLocation() {
+
+        Location location = null;
+
+        if (manager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) ||
+                manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            if (manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return null;
+                }
+                location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            }
+        } else {
+            Intent locIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivityForResult(locIntent, REQUEST_LOCATION);
+        }
+        return location;
+    }
+
+    public void updateMap(Location initialLoc, Location mostUpdatedLoc) {
+        initialLocation = initialLoc;
+        mostUpdateLocation = mostUpdatedLoc;
+
+        if (mostUpdateLocation != null) {
+            latLng = new LatLng(mostUpdateLocation.getLatitude(), mostUpdateLocation.getLongitude());
+            mMap.clear();
+            coordinates = "(" + mostUpdateLocation.getLatitude() + ";" + mostUpdateLocation.getLongitude() + ")";
+            mMap.addMarker(new MarkerOptions().position(latLng).title(coordinates));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, ZOOM_LEVEL));
+        }
+    }
+
+}
